d16617bc9a52d798431ab35aeb2917d077b1c864
houqian520 <115726508+houqian520@users.noreply.github.com>
2022-10-14 20:39:06 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,43 +19,43 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '倩毛',
+      name: '倩毛毛',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'o-PYE6R_egunO4EFNnuQpImaXYxs',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'o-PYE6R_egunO4EFNnuQpImaXYxs',
+      useTemplateId: '	J9Fp2F4kofWk6h5O9jc921Uic8mY8Dag1lal1dvXLGo',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-24',
+      horoscopeDate: '01-26',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '倩毛毛', year: '2002', date: '12-14',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '倩毛', year: '2002', date: '12-14',
+          type: '生日', name: '鹏鹏', year: '2002', date: '04-25',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2000', date: '06-06',
+          type: '节日', name: '恋爱纪念日', year: '2002', date: '05-04',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
+      customizedDateList: [1226
         // 在一起的日子
         { keyword: 'love_day', date: '2019-06-06' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        // 结婚前纪念日
+        { keyword: 'marry_day', date: '2024-06-06' },
       ],
     },
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'n7JId8mPJ0XgtjxiZJx2U5wIju_joZNHnCLYO78DZKA',
+  CALLBACK_TEMPLATE_ID: 'n9Fp2F4kofWk6h5O9jc921Uic8mY8Dag1lal1dvXLGo',
 
   CALLBACK_USERS: [
     {
-      name: '自己',
+      name: '鹏鹏',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o-PYE6R_egunO4EFNnuQpImaXYxs',
     }
