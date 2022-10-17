4d45c5ad7cc29b09a8db299a4eeb901d9002a485
ailiyueaa <115940549+ailiyueaa@users.noreply.github.com>
2022-10-17 20:37:20 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -33,7 +33,7 @@ const USER_CONFIG = {
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '黑驴蛋蛋', year: '2002', date: '02-16',
+          type: '生日', name: '豪杰', year: '2002', date: '02-16',
         },
         {
           type: '节日', name: '恋爱纪念日', year: '2015', date: '01-04',
