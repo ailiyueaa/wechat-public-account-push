8fa5f5154ae1a1e406b8682fb9530683655f3871
ailiyueaa <115940549+ailiyueaa@users.noreply.github.com>
2022-10-17 20:35:51 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '悦悦',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'osixz6g7XmLVZkn_uVBF9OBkL_ik',
+      id: 'osixz6j1MdkcVpt6hrVgmTotABi0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'a9UARrb73DUYpnGUXa2UVtt_7uQLw-Qr-AhuXaJizo4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
