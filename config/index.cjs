7e8daa2f15af53180b21dd81fa88ed8f85982c37
ailiyueaa <115940549+ailiyueaa@users.noreply.github.com>
2022-10-17 20:28:58 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'osixz6g7XmLVZkn_uVBF9OBkL_ik',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'de9gfGlXxdVub7OOie9EekwzNiZoQ4dGP58W_UAXOl0',
+      useTemplateId: 'uNXW9-M5gzLY7wFbcd7MG743x2Bi_CCZmxgadY0-_04',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '04-02',
       festivals: [
@@ -60,7 +60,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'de9gfGlXxdVub7OOie9EekwzNiZoQ4dGP58W_UAXOl0',
+  CALLBACK_TEMPLATE_ID: 'uNXW9-M5gzLY7wFbcd7MG743x2Bi_CCZmxgadY0-_04',
 
   CALLBACK_USERS: [
     {
